8fccb8f324a733c39869350afedb13eae09c1f0f
Kevin Baldwyn <kevin.baldwyn@edfenergy.com>
2025-01-07 16:50:42 +0000

    fix: improve type definitions for Matchers

--- src/Nodes/Matcher.ts
+++ src/Nodes/Matcher.ts
@@ -12,58 +12,52 @@ export type NodeMatcher<NodeType extends Node> = NodeMatchFn<boolean, NodeType>;
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class Matcher {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static node: Record<string, (...args: any[]) => NodeMatchFn<boolean, Node>> =
-    {
-      labelStartsWith: (startsWith: string[]): NodeMatchFn<boolean, Node> => {
-        return (nodeName, node) => {
-          for (const str of startsWith) {
-            if (node.label.startsWith(str)) {
-              return true;
-            }
+  static node = {
+    labelStartsWith: (startsWith: string[]): NodeMatchFn<boolean, Node> => {
+      return (nodeName, node) => {
+        for (const str of startsWith) {
+          if (node.label.startsWith(str)) {
+            return true;
           }
-          return false;
-        };
-      },
-      resourceEquals: (values: string[]): NodeMatcher<NodeWithMeta> => {
-        return (nodeName, node) => {
-          return values.includes(node.meta?.resource ?? '');
-        };
-      },
-      nodeNameEquals: (values: string[]): NodeMatcher<NodeWithMeta> => {
-        return (nodeName) => {
-          return values.includes(nodeName);
-        };
-      },
-      resourceOrNodeNameEquals: (
-        values: string[],
-      ): NodeMatcher<NodeWithMeta> => {
-        return (nodeName, node) => {
-          return (
-            values.includes(node.meta?.resource ?? '') ||
-            values.includes(nodeName)
-          );
-        };
-      },
-    };
+        }
+        return false;
+      };
+    },
+    resourceEquals: (values: string[]): NodeMatcher<NodeWithMeta> => {
+      return (nodeName, node) => {
+        return values.includes(node.meta?.resource ?? '');
+      };
+    },
+    nodeNameEquals: (values: string[]): NodeMatcher<NodeWithMeta> => {
+      return (nodeName) => {
+        return values.includes(nodeName);
+      };
+    },
+    resourceOrNodeNameEquals: (values: string[]): NodeMatcher<NodeWithMeta> => {
+      return (nodeName, node) => {
+        return (
+          values.includes(node.meta?.resource ?? '') ||
+          values.includes(nodeName)
+        );
+      };
+    },
+  };
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static edge: Record<string, (...args: any[]) => NodeMatchFn<boolean, Node>> =
-    {
-      fromTo: (
-        matchers: {
-          from: NodeMatchFn<boolean, Node>;
-          to: NodeMatchFn<boolean, Node>;
-        }[],
-      ) => {
-        return (nodeName, node, graph) => {
-          for (const matcher of matchers) {
-            if (matcher.from(nodeName, node, graph)) {
-              return true;
-            }
+  static edge = {
+    fromTo: (
+      matchers: {
+        from: NodeMatchFn<boolean, Node>;
+        to: NodeMatchFn<boolean, Node>;
+      }[],
+    ): NodeMatchFn<boolean, Node> => {
+      return (nodeName, node, graph) => {
+        for (const matcher of matchers) {
+          if (matcher.from(nodeName, node, graph)) {
+            return true;
           }
-          return false;
-        };
-      },
-    };
+        }
+        return false;
+      };
+    },
+  };
 }
